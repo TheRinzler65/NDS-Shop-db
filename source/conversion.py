7e32ededb81d233638a6c53e306279197d03ea38
Rinzler <119778544+TheRinzler65@users.noreply.github.com>
2024-08-19 21:50:20 +0200

    change language for this file

--- source/conversion.py
+++ source/conversion.py
@@ -1,10 +1,10 @@
-share_link = input("Entrez le lien de partage Google Drive : ")
+share_link = input("Enter the Google Drive share link: ")
 try:
     file_id = share_link.split('/d/')[1].split('/')[0]
     
 
     direct_link = f"https://drive.google.com/uc?export=download&id={file_id}"
     
-    print("Lien direct de téléchargement : ", direct_link)
+    print("Direct download link: ", direct_link)
 except IndexError:
-    print("Lien invalide. Assurez-vous que le lien est correct.")
+    print("Invalid link. Please make sure the link is correct.")
